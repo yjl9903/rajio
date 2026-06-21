8d2832efd84453a6603ca23f8dd9c63f2e1b8f83
yjl9903 <yjl9903@onekuma.cn>
2026-06-22 01:14:47 +0800

    fix: add charset header

--- apps/landing/src/routes/__root.tsx
+++ apps/landing/src/routes/__root.tsx
@@ -8,11 +8,14 @@ export const Route = createRootRoute({
   head: () => ({
     meta: [
       {
-        title: 'Rajio'
+        charSet: 'utf-8'
       },
       {
         name: 'viewport',
         content: 'width=device-width, initial-scale=1.0'
+      },
+      {
+        title: 'Rajio'
       }
     ]
   }),
